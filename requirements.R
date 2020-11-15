48ab8f62cbcf78df8c5a6f6f0d28961d4f586efe
Eric Bryant <ericbryantphd@gmail.com>
2020-11-15 10:07:36 -0800

    Remove minor horizontal grid lines

--- requirements.R
+++ requirements.R
@@ -5,6 +5,9 @@ library(slider)
 source(here("code/R/download-new-cases-ca.R"))
 source(here("code/R/figure-new-cases.R"))
 
-theme_set(theme_minimal())
+theme_set(
+  theme_minimal() +
+    theme(panel.grid.minor.y = element_blank())
+)
 
 knitr::opts_chunk$set(echo = FALSE, message = FALSE)
