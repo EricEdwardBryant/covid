27e8a3f058daef41a8dc10e930afc8efca1a69eb
Eric Bryant <ericbryantphd@gmail.com>
2020-11-20 09:36:40 -0800

    Add documentation to download_new_cases_ca()

--- code/R/download-new-cases-ca.R
+++ code/R/download-new-cases-ca.R
@@ -1,12 +1,26 @@
 #' Download COVID case data for CA
 #'
+#' @param save_as
+#' @param force Whether to force a download even if the data were downloaded
+#' today. Defaults to `FALSE` to prevent from accidentally spamming the API.
+#' @param api URL to the [https://data.ca.gov] API.
+#' @param dataset ID for the dataset to query.
+#' @param sql_select Columns to retrieve as an SQL string. Defaults to all
+#' columns i.e.`"*"`.
+#' @param sql_where Rows to retrieve as an SQL "WHERE" clause. Defaults to rows
+#' without a missing date i.e. `'"date" IS NOT NULL'`. To get e.g. just LA
+#' county, you can use `"\"county\" = 'Los Angeles'"`.
 #'
+#' @details Constructs a URL with an SQL query to request data covid data from
+#' [https://data.ca.gov]. By default, data will only be downloaded if the
+#' save as target (specifically the `.json` version) was not already downloaded
+#' today.
 
 download_new_cases_ca <- function(save_as,
                                   force = FALSE,
                                   api = "https://data.ca.gov/api/3/action/datastore_search_sql?sql=",
-                                  sql_select = "*",
                                   dataset = "926fd08f-cc91-4828-af38-bd45de97f8c3",
+                                  sql_select = "*",
                                   sql_where = '"date" IS NOT NULL') {
   # Where to download the data
   json <- str_c(tools::file_path_sans_ext(save_as), ".json")
