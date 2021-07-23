7f40599bd05828b65c26191d46947a2889cdfd7d
Eric Bryant <ericbryantphd@gmail.com>
2021-07-23 10:58:47 -0700

    Fix for new blogdown build_site behavior

--- make.R
+++ make.R
@@ -1,4 +1,3 @@
 if (file.exists("content/_index.html")) file.remove("content/_index.html")
-blogdown::build_site(local = TRUE)
-servr::daemon_stop()
-servr::httd("public")
+blogdown::stop_server()
+blogdown::serve_site()
