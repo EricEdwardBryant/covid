08e2523612c390958f56f20b080b9c87597a85dd
Eric Bryant <ericbryantphd@gmail.com>
2021-12-22 17:56:32 -0800

    Add table

--- install.R
+++ install.R
@@ -4,3 +4,5 @@ local({
   hugo <- "0.61.0"
   if (blogdown::hugo_version() != hugo) blogdown::install_hugo(hugo, force = TRUE)
 })
+
+if (!webshot::is_phantomjs_installed()) webshot::install_phantomjs()
