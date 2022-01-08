1f6a3cc14260e6d8cc8c70ae1dee07c3abdbcf11
Eric Bryant <ericbryantphd@gmail.com>
2022-01-07 16:21:46 -0800

    Add comments to install.R

--- install.R
+++ install.R
@@ -1,8 +1,18 @@
+# Installs packages recorded in renv.lock
 renv::restore()
 
+# External (non-R) dependencies
 local({
+  # Hugo is the static website builder
   hugo <- "0.61.0"
-  if (blogdown::hugo_version() != hugo) blogdown::install_hugo(hugo, force = TRUE)
+  if (blogdown::hugo_version() != hugo) {
+    blogdown::install_hugo(hugo, force = TRUE)
+  }
+
+  # PhantomJS is required to generate PNGs of gt generated html tables
+  if (!webshot::is_phantomjs_installed()) {
+    webshot::install_phantomjs()
+  }
 })
 
-if (!webshot::is_phantomjs_installed()) webshot::install_phantomjs()
+
